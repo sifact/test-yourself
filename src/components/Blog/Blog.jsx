fec4fdac1a43ac847dc3837bb87ab4b6c761bb8a
sifat <Sifatami596@gmail.com>
2022-10-12 22:53:13 +0600

    blog completed

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -6,7 +6,7 @@ import img3 from "./hook.png";
 
 const Blog = () => {
     return (
-        <section>
+        <section className="box__wrapper">
             <div className="box container">
                 <div className="card">
                     <img src={img1} alt="" />
@@ -19,7 +19,7 @@ const Blog = () => {
                             the page refreshing as the user navigates. It uses
                             component structure to call components, which
                             display the appropriate information.
-                            <h1></h1>
+                            <div className="space"></div>
                             It is also use for Active Link, data loading, data
                             mutations.
                         </p>
@@ -28,10 +28,20 @@ const Blog = () => {
                 <div className="card">
                     <img src={img2} alt="" />
                     <div className="content">
-                        <h1>Context Api</h1>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quisquam accusamus eveniet soluta recusandae laudantium
-                        repellendus neque ex nihil qui sunt asperiores aliquam
+                        <h1>How React Context API works</h1>
+                        The React Context API is a way for a React app to
+                        effectively produce global variables that can be passed
+                        around. This is the alternative to "prop drilling" or
+                        moving props from grandparent to child to parent, and so
+                        on.
+                        <div className="space"></div>
+                        Context API is created using React.createContext(). It
+                        returns a consumer and a provider. Provider is a
+                        component that as it's names suggests provides the state
+                        to its children. It will hold the "store" and be the
+                        parent of all the components that might need that store.
+                        Consumer as it happens is a component that consumes and
+                        uses the state.
                     </div>
                 </div>
                 <div className="card">
@@ -43,7 +53,7 @@ const Blog = () => {
                         returns a mutable ref object. This object has a property
                         called .current. The value is persisted in the
                         refContainer.current property.
-                        <h1></h1>
+                        <div className="space"></div>
                         The .current property could be initialized to the passed
                         argument initialValue for instance,
                         useRef(initialValue). The object can persist a value for
